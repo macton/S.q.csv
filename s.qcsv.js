573870ab7351718164024ac28a1d316b5e8d6153
Mike Acton <macton@gmail.com>
2015-06-22 15:51:48 -0700

    curry

--- s.qcsv.js
+++ s.qcsv.js
@@ -5,7 +5,7 @@ var csv = require('csv');
 
 var qcsv = {
   load  : function( filename ) { return Q.ninvoke( fs, 'readFile', filename, 'utf8' ).then( function( data ) { return Q.ninvoke( csv, 'parse', data ); } ); },
-  save  : function( filename, rows ) { return Q.ninvoke( csv, 'stringify', rows ) .then( function( text ) { return Q.ninvoke( fs, 'writeFile', filename, text, 'utf8' ); } ) },
+  save  : S.curry( function( filename, rows ) { return Q.ninvoke( csv, 'stringify', rows ) .then( function( text ) { return Q.ninvoke( fs, 'writeFile', filename, text, 'utf8' ); } ) } ),
 };
 
 exports = module.exports = qcsv;
